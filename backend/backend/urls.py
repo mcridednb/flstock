322bbbe459374674bb5f3e3fa0a4c6f5534be953
nikolai <mcridednb@gmail.com>
2024-05-30 14:16:03 -0300

    fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -36,8 +36,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    # path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    # path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
