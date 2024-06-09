8f672ea7e07bae8a6696e65c12677608662fa2b0
nikolai <mcridednb@gmail.com>
2024-06-09 16:05:28 -0300

    + design category FL

--- parser/spiders/fl/constants.py
+++ parser/spiders/fl/constants.py
@@ -3,23 +3,28 @@ SITES = [
         "id": 85,
         "title": "Копирайтинг",
         "code": "writing",
-    }, {
+    },
+    {
         "id": 3,
         "title": "Дизайн сайтов",
         "code": "design",
-    }, {
+    },
+    {
         "id": 164,
         "title": "Контент-менеджер",
         "code": "writing",
-    }, {
+    },
+    {
         "id": 84,
         "title": "Менеджер проектов",
         "code": "consulting",
-    }, {
+    },
+    {
         "id": 502,
         "title": "Редизайн сайтов",
         "code": "design",
-    }, {
+    },
+    {
         "id": 470,
         "title": "Пользовательские соглашения",
         "code": "consulting",
@@ -29,196 +34,319 @@ SITES = [
 CATEGORIES = [{
     "title": "Разработка и IT",
     "code": "programming",
-    "subcategories": [{
-        "id": 37,
-        "title": "Веб-программирование",
-        "code": "web-development",
-    }, {
-        "id": 1,
-        "title": "Прикладное программирование",
-        "code": "desktop-development",
-    }, {
-        "id": 279,
-        "title": "Разработка Чат-ботов",
-        "code": "script-development",
-    }, {
-        "id": 225,
-        "title": "Google Android",
-        "code": "mobile-development",
-    }, {
-        "id": 97,
-        "title": "1С-программирование",
-        "code": "other-development",
-    }, {
-        "id": 280,
-        "title": "Парсинг данных",
-        "code": "script-development",
-    }, {
-        "id": 5,
-        "title": "Базы данных",
-        "code": "databases-development",
-    }, {
-        "id": 226,
-        "title": "iOS",
-        "code": "mobile-development",
-    }, {
-        "id": 297,
-        "title": "Создание скриптов",
-        "code": "script-development",
-    }, {
-        "id": 2,
-        "title": "Системное программирование",
-        "code": "desktop-development",
-    }, {
-        "id": 222,
-        "title": "Разработка CRM и ERP",
-        "code": "other-development",
-    }, {
-        "id": 6,
-        "title": "Программирование для сотовых телефонов и КПК",
-        "code": "mobile-development",
-    }, {
-        "id": 296,
-        "title": "Машинное обучение",
-        "code": "ai-blockchain",
-    }, {
-        "id": 221,
-        "title": "Плагины/Сценарии/Утилиты",
-        "code": "script-development",
-    }, {
-        "id": 223,
-        "title": "Интерактивные приложения",
-        "code": "web-development",
-    }, {
-        "id": 161,
-        "title": "Встраиваемые системы",
-        "code": "other-development",
-    }, {
-        "id": 278,
-        "title": "Blockchain",
-        "code": "ai-blockchain",
-    }, {
-        "id": 229,
-        "title": "Прототипирование",
-        "code": "other-development",
-    }, {
-        "id": 473,
-        "title": "Laravel",
-        "code": "web-development",
-    }, {
-        "id": 474,
-        "title": "Vue",
-        "code": "web-development",
-    }, {
-        "id": 355,
-        "title": "Yii",
-        "code": "web-development",
-    }, {
-        "id": 9,
-        "title": "Веб-программирование",
-        "code": "web-development",
-    }, {
-        "id": 27,
-        "title": "Сайт «под ключ»",
-        "code": "web-development",
-    }, {
-        "id": 8,
-        "title": "Верстка",
-        "code": "web-development",
-    }, {
-        "id": 282,
-        "title": "Тильда",
-        "code": "web-development",
-    }, {
-        "id": 276,
-        "title": "Лендинги",
-        "code": "web-development",
-    }, {
-        "id": 81,
-        "title": "QA (тестирование)",
-        "code": "testing-audit",
-    }, {
-        "id": 217,
-        "title": "Интернет-магазины",
-        "code": "web-development",
-    }, {
-        "id": 275,
-        "title": "WordPress",
-        "code": "web-development",
-    }, {
-        "id": 220,
-        "title": "Доработка сайтов",
-        "code": "web-development",
-    }, {
-        "id": 86,
-        "title": "CMS (системы управления)",
-        "code": "web-development",
-    }, {
-        "id": 218,
-        "title": "Проектирование",
-        "code": "web-development",
-    }, {
-        "id": 281,
-        "title": "1С Битрикс",
-        "code": "web-development",
-    }, {
-        "id": 307,
-        "title": "Zero-coding",
-        "code": "web-development",
-    }, {
-        "id": 349,
-        "title": "React",
-        "code": "web-development",
-    }, {
-        "id": 219,
-        "title": "Юзабилити-анализ",
-        "code": "testing-audit",
-    }, {
-        "id": 72,
-        "title": "Wap/PDA-сайты",
-        "code": "web-development",
-    }, {
-        "id": 87,
-        "title": "Флеш-сайты",
-        "code": "web-development",
-    }, {
-        "id": 347,
-        "title": "Laravel",
-        "code": "web-development",
-    }, {
-        "id": 348,
-        "title": "Vue",
-        "code": "web-development",
-    }, {
-        "id": 350,
-        "title": "Node",
-        "code": "web-development",
-    }, {
-        "id": 353,
-        "title": "Joomla",
-        "code": "web-development",
-    }, {
-        "id": 443,
-        "title": "Webflow",
-        "code": "web-development",
-    }, {
-        "id": 351,
-        "title": "OpenCart",
-        "code": "web-development",
-    }, {
-        "id": 352,
-        "title": "MODx",
-        "code": "web-development",
-    }, {
-        "id": 354,
-        "title": "Magento",
-        "code": "web-development",
-    }, {
-        "id": 475,
-        "title": "Yii",
-        "code": "web-development",
-    }],
-}]
+    "subcategories": [
+        {
+            "id": 37,
+            "title": "Веб-программирование",
+            "code": "web-development",
+        },
+        {
+            "id": 1,
+            "title": "Прикладное программирование",
+            "code": "desktop-development",
+        },
+        {
+            "id": 279,
+            "title": "Разработка Чат-ботов",
+            "code": "script-development",
+        },
+        {
+            "id": 225,
+            "title": "Google Android",
+            "code": "mobile-development",
+        },
+        {
+            "id": 97,
+            "title": "1С-программирование",
+            "code": "other-development",
+        },
+        {
+            "id": 280,
+            "title": "Парсинг данных",
+            "code": "script-development",
+        },
+        {
+            "id": 5,
+            "title": "Базы данных",
+            "code": "databases-development",
+        },
+        {
+            "id": 226,
+            "title": "iOS",
+            "code": "mobile-development",
+        },
+        {
+            "id": 297,
+            "title": "Создание скриптов",
+            "code": "script-development",
+        },
+        {
+            "id": 2,
+            "title": "Системное программирование",
+            "code": "desktop-development",
+        },
+        {
+            "id": 222,
+            "title": "Разработка CRM и ERP",
+            "code": "other-development",
+        },
+        {
+            "id": 6,
+            "title": "Программирование для сотовых телефонов и КПК",
+            "code": "mobile-development",
+        },
+        {
+            "id": 296,
+            "title": "Машинное обучение",
+            "code": "ai-blockchain",
+        },
+        {
+            "id": 221,
+            "title": "Плагины/Сценарии/Утилиты",
+            "code": "script-development",
+        },
+        {
+            "id": 223,
+            "title": "Интерактивные приложения",
+            "code": "web-development",
+        },
+        {
+            "id": 161,
+            "title": "Встраиваемые системы",
+            "code": "other-development",
+        },
+        {
+            "id": 278,
+            "title": "Blockchain",
+            "code": "ai-blockchain",
+        },
+        {
+            "id": 229,
+            "title": "Прототипирование",
+            "code": "other-development",
+        },
+        {
+            "id": 473,
+            "title": "Laravel",
+            "code": "web-development",
+        },
+        {
+            "id": 474,
+            "title": "Vue",
+            "code": "web-development",
+        },
+        {
+            "id": 355,
+            "title": "Yii",
+            "code": "web-development",
+        },
+        {
+            "id": 9,
+            "title": "Веб-программирование",
+            "code": "web-development",
+        },
+        {
+            "id": 27,
+            "title": "Сайт «под ключ»",
+            "code": "web-development",
+        },
+        {
+            "id": 8,
+            "title": "Верстка",
+            "code": "web-development",
+        },
+        {
+            "id": 282,
+            "title": "Тильда",
+            "code": "web-development",
+        },
+        {
+            "id": 276,
+            "title": "Лендинги",
+            "code": "web-development",
+        },
+        {
+            "id": 81,
+            "title": "QA (тестирование)",
+            "code": "testing-audit",
+        },
+        {
+            "id": 217,
+            "title": "Интернет-магазины",
+            "code": "web-development",
+        },
+        {
+            "id": 275,
+            "title": "WordPress",
+            "code": "web-development",
+        },
+        {
+            "id": 220,
+            "title": "Доработка сайтов",
+            "code": "web-development",
+        },
+        {
+            "id": 86,
+            "title": "CMS (системы управления)",
+            "code": "web-development",
+        },
+        {
+            "id": 218,
+            "title": "Проектирование",
+            "code": "web-development",
+        },
+        {
+            "id": 281,
+            "title": "1С Битрикс",
+            "code": "web-development",
+        },
+        {
+            "id": 307,
+            "title": "Zero-coding",
+            "code": "web-development",
+        },
+        {
+            "id": 349,
+            "title": "React",
+            "code": "web-development",
+        },
+        {
+            "id": 219,
+            "title": "Юзабилити-анализ",
+            "code": "testing-audit",
+        },
+        {
+            "id": 72,
+            "title": "Wap/PDA-сайты",
+            "code": "web-development",
+        },
+        {
+            "id": 87,
+            "title": "Флеш-сайты",
+            "code": "web-development",
+        },
+        {
+            "id": 347,
+            "title": "Laravel",
+            "code": "web-development",
+        },
+        {
+            "id": 348,
+            "title": "Vue",
+            "code": "web-development",
+        },
+        {
+            "id": 350,
+            "title": "Node",
+            "code": "web-development",
+        },
+        {
+            "id": 353,
+            "title": "Joomla",
+            "code": "web-development",
+        },
+        {
+            "id": 443,
+            "title": "Webflow",
+            "code": "web-development",
+        },
+        {
+            "id": 351,
+            "title": "OpenCart",
+            "code": "web-development",
+        },
+        {
+            "id": 352,
+            "title": "MODx",
+            "code": "web-development",
+        },
+        {
+            "id": 354,
+            "title": "Magento",
+            "code": "web-development",
+        },
+        {
+            "id": 475,
+            "title": "Yii",
+            "code": "web-development",
+        }
+    ],
+},
+    {
+        "title": "Дизайн",
+        "code": "design",
+        "subcategories": [
+            {"id": 46, "title": "Логотипы", "code": "design-branding"},
+            {"id": 3, "title": "Дизайн сайтов", "code": "design-web-mobile"},
+            {"id": 172, "title": "Дизайн сайтов", "code": "design-web-mobile"},
+            {"id": 42, "title": "Фирменный стиль", "code": "design-branding"},
+            {"id": 18, "title": "Фирменный стиль", "code": "design-branding"},
+            {"id": 49, "title": "Полиграфический дизайн", "code": "design-layout"},
+            {"id": 300, "title": "UI/UX дизайн", "code": "design-interfaces"},
+            {"id": 19, "title": "Презентации", "code": "design-presentations"},
+            {"id": 517, "title": "Презентации", "code": "design-presentations"},
+            {"id": 51, "title": "Дизайн упаковки", "code": "design-web-mobile"},
+            {"id": 238, "title": "Инфографика", "code": "design-presentations"},
+            {"id": 444, "title": "Figma", "code": "design-web-mobile"},
+            {"id": 35, "title": "Интерфейсы", "code": "design-interfaces"},
+            {"id": 45, "title": "Баннеры", "code": "design-outdoor"},
+            {"id": 146, "title": "Интерьеры", "code": "design-interior-exterior"},
+            {"id": 50, "title": "Интерьеры", "code": "design-interior-exterior"},
+            {"id": 283, "title": "Лэндинги", "code": "design-web-mobile"},
+            {"id": 239, "title": "Мобильные приложения", "code": "design-web-mobile"},
+            {"id": 502, "title": "Редизайн сайтов", "code": "design-web-mobile"},
+            {"id": 397, "title": "Редизайн сайтов", "code": "design-web-mobile"},
+            {"id": 500, "title": "Дизайн для соцсетей", "code": "design-social-media"},
+            {"id": 93, "title": "Архитектура", "code": "design-interior-exterior"},
+            {"id": 90, "title": "Полиграфическая верстка", "code": "design-layout"},
+            {"id": 52, "title": "Промышленный дизайн", "code": "design-branding"},
+            {"id": 414, "title": "Промышленный дизайн", "code": "design-branding"},
+            {"id": 78, "title": "Разработка шрифтов", "code": "design-fonts"},
+            {"id": 75, "title": "Наружная реклама", "code": "design-outdoor"},
+            {"id": 501, "title": "Дизайн для маркетплейсов", "code": "design-social-media"},
+            {"id": 143, "title": "Ландшафтный дизайн/Генплан", "code": "design-interior-exterior"},
+            {"id": 128, "title": "Дизайн выставочных стендов", "code": "design-outdoor"},
+            {"id": 273, "title": "Верстка электронных изданий", "code": "design-layout"},
+            {"id": 91, "title": "2D Персонажи", "code": "design-art"},
+            {"id": 168, "title": "3D Иллюстрации", "code": "design-3d-graphics"},
+            {"id": 306, "title": "NFT-арт", "code": "design-art"},
+            {"id": 15, "title": "Векторная графика", "code": "design-art"},
+            {"id": 190, "title": "Визуализация/3D", "code": "design-3d-graphics"},
+            {"id": 47, "title": "Иконки", "code": "design-web-mobile"},
+            {"id": 379, "title": "Книжные иллюстрации", "code": "design-art"},
+            {"id": 88, "title": "Комиксы", "code": "design-art"},
+            {"id": 383, "title": "Обложки для видео", "code": "design-social-media"},
+            {"id": 409, "title": "Обложки для соцсетей", "code": "design-social-media"},
+            {"id": 157, "title": "Пиксел-арт", "code": "design-art"},
+            {"id": 48, "title": "Рисунки и иллюстрации", "code": "design-art"},
+            {"id": 400, "title": "Техническая иллюстрация", "code": "design-art"},
+            {"id": 522, "title": "3D графика", "code": "design-3d-graphics"},
+            {"id": 518, "title": "Email оформление", "code": "design-branding"},
+            {"id": 527, "title": "POS-материалы", "code": "design-branding"},
+            {"id": 531, "title": "Брендбук", "code": "design-branding"},
+            {"id": 523, "title": "Визитки", "code": "design-branding"},
+            {"id": 530, "title": "Гайдлайн", "code": "design-branding"},
+            {"id": 519, "title": "Корпоративная графика", "code": "design-branding"},
+            {"id": 529, "title": "Логобук", "code": "design-branding"},
+            {"id": 513, "title": "Логотип", "code": "design-branding"},
+            {"id": 514, "title": "Маскот", "code": "design-branding"},
+            {"id": 528, "title": "Мокапы", "code": "design-branding"},
+            {"id": 510, "title": "Нейминг", "code": "design-branding"},
+            {"id": 524, "title": "Открытки/приглашения", "code": "design-branding"},
+            {"id": 526, "title": "Пластиковые карты", "code": "design-branding"},
+            {"id": 525, "title": "Полиграфия", "code": "design-branding"},
+            {"id": 517, "title": "Презентации", "code": "design-branding"},
+            {"id": 511, "title": "Слоганы/дескрипторы", "code": "design-branding"},
+            {"id": 521, "title": "Типографика", "code": "design-layout"},
+            {"id": 512, "title": "Товарный знак", "code": "design-branding"},
+            {"id": 533, "title": "Упаковка", "code": "design-layout"},
+            {"id": 532, "title": "Фирменная одежда", "code": "design-branding"},
+            {"id": 520, "title": "Фотостиль", "code": "design-branding"},
+            {"id": 515, "title": "Цветовая схема", "code": "design-branding"},
+            {"id": 516, "title": "Шрифты", "code": "design-fonts"},
+        ]
+    }
+]
 
 SUBCATEGORIES_REVERSE = {
     subcategory["id"]: subcategory["code"]
